37140295f9faa0ac6c4c2b30de64d7016bc55c7a
jesusfluna <luna1988_88@hotmail.com>
2022-09-20 12:42:00 +0200

    Rectangulo por array

--- 03-Rects.py
+++ 03-Rects.py
@@ -9,13 +9,17 @@ surface = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Hola PyGame")
 
 red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 255, 0)
 white = pygame.Color(255, 255, 255)
 
-#Creacion de un rectangulo en pygame
-rect = pygame.Rect(100, 150, 120, 60) # posX, posY, width, height
-rect.center = (width//2, height//2) #Centramos el rectangulo a mitad de la pantalla
+# Creación de un rectángulo en pygame
+rect = pygame.Rect(100, 150, 120, 60)  # posX, posY, width, height
+rect.center = (width//2, height//2)  # Centramos el rectángulo a mitad de la pantalla
 print(rect.x)
 print(rect.y)
+
+# Creación de un rectángulo con arrays
+rect2 = (100, 100, 80, 40) # posX, posY, width, height
 
 while True:
 
@@ -26,7 +30,8 @@ while True:
 
     surface.fill(white)
 
-    #Pintamos el rectangulo
-    pygame.draw.rect(surface, red, rect)# surface, color, rectangulo
+    # Pintamos el rectángulo
+    pygame.draw.rect(surface, red, rect)  # surface, color, rectángulo
+    pygame.draw.rect(surface, green, rect2)  # surface, color, rectángulo
 
     pygame.display.update()
